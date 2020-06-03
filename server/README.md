508effaf76399e89ab75f04cd293c8da4fac5c2f
gustavo867 <tartatavo3@gmail.com>
2020-06-03 03:29:20 -0300

    back-end

---
+++ server/README.md
@@ -0,0 +1,5 @@
+## NEXT LEVEL WEEK BOOSTER
+
+## Back-End Finalizado
+
+![image](https://user-images.githubusercontent.com/63013756/83603138-3eeb6a80-a54a-11ea-86e2-b48a58fbe92c.png)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
+import cors from 'cors'
+import routes from './routes';
+import path from 'path'
 
 const app = express();
 
-app.get('/users', (request, response) => {
-   response.json([
-       'Diego',
-       'Cleiton',
-       'Robson',
-       'Daniel'
-   ]);
-})
+app.use(cors());
+app.use(express.json())
+app.use(routes);
 
-app.listen(3333)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads' )));
+
+app.listen(3333);

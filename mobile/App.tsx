9ef1855d77425801f74b245210fe88940a430c4e
gustavo867 <tartatavo3@gmail.com>
2020-06-06 05:04:06 -0300

    Tentando finalizar mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { AppLoading } from 'expo'
 import { StatusBar } from "react-native";
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
-import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
-import Routes from './src/routes'
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
